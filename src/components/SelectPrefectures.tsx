62680eed1092c1ee5c6221470e046f6cfa30a2f1
ihch <nemu0sou@gmail.com>
2022-01-13 23:18:44 +0900

    feat: 出発地と目的地の都道府県を選択するコンポーネントを追加

---
+++ src/components/SelectPrefectures.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Pulldown, { PulldownItem } from './Pulldown';
+
+type SelectPrefecturesProps = {
+  prefectures: PulldownItem[];
+  onChangeDeparture: (v: any) => void;
+  onChangeDestination: (v: any) => void;
+};
+
+const SelectPrefectures: React.VFC<SelectPrefecturesProps> = ({
+  prefectures,
+  onChangeDeparture,
+  onChangeDestination
+}) => {
+  return (
+    <div className='flex items-center mt-8'>
+      <Pulldown items={prefectures} placeholder='出発地' onChange={onChangeDeparture} />
+      <span className='text-4xl leading-none inline-block align-middle mx-2'>-</span>
+      <Pulldown items={prefectures} placeholder='目的地' onChange={onChangeDestination} />
+    </div>
+  )
+}
+
+export default SelectPrefectures;
