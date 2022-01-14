d913aa92ec56dbadabd15da053398578f98cc563
ihch <nemu0sou@gmail.com>
2022-01-15 02:48:07 +0900

    fix: いらないもの削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 
 function App() {
-  const [prefectures, setPrefectures] = useState([]);
-
-  useEffect(() => {
-    const f = () => {
-      fetch("/prefectures")
-        .then((response) => {
-          return response.json();
-        })
-        .then((value) => setPrefectures(value));
-    };
-
-    f();
-  }, []);
-
-  useEffect(() => {
-    console.log(prefectures);
-  }, [prefectures]);
-
   return (
     <div className="App">
       <Header />
